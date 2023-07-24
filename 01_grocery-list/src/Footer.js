109282183beef5c4ff57b8b4cf7b4faf65492b16
andhiyaulhaq <69621271+andhiyaulhaq@users.noreply.github.com>
2023-07-24 15:10:17 +0700

    upload first project
    grocery list react app

---
+++ 01_grocery-list/src/Footer.js
@@ -0,0 +1,14 @@
+const Footer = ({ length }) => {
+  const today = new Date();
+
+  return (
+    <footer>
+      <p>
+        {length} List {length !== 1 ? "items" : "item"} | Copyright &copy;{" "}
+        {today.getFullYear()}{" "}
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
